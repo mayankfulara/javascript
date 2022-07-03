3f1ce0a547de4ff4c795f4c74c3e59515ce6258a
mayankfulara <40884356+mayankfulara@users.noreply.github.com>
2022-07-04 00:21:42 +0530

    Add files via upload

---
+++ zK0yNKvpSRStMjSr6UkUCg_dcf056afd22d48edaa5addb239db3bc6_m4.1---BasicConverter-Start/script.js
@@ -0,0 +1,15 @@
+(function(){
+    'use strict';
+    document.getElementById('convert').addEventListener('submit',function(event){
+        event.preventDefault();
+        const distance=parseFloat(document.getElementById('distance').value);
+        const answer=document.getElementById('answer');
+        if(distance){
+            const conversion= (distance*1.609344).toFixed(4)
+            answer.innerHTML=`<h2>${distance} miles convert to ${conversion} kilometers</h2>`
+        }
+        else{
+            answer.innerHTML='<h2>Please provide a number!!!</h2>'
+        }
+    })
+})();
